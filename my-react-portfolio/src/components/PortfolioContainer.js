fa489f5c5ef195ca908cacda7fe9c7974543d320
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 12:17:17 -0400

    added come initial code

---
+++ my-react-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+          return <About />;
+        }
+        if (currentPage === 'Portfolio') {
+          return <Portfolio />;
+        }
+        if (currentPage === 'Contact') {
+          return <Contact />;
+        }
+    }
+};
