e8c24fefb8918ba7e62c34bb11ca4c301007729f
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 16:07:44 -0400

    started form for contact page

--- my-react-portfolio/src/components/pages/Contact.js
+++ my-react-portfolio/src/components/pages/Contact.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
 
- export default function Contact() {
-   return (
-     <div>
-       <h1>Contact</h1>
-       <p>
-         Contact me.
-       </p>
-     </div>
-   );
- }
+function Form() {
+  const [email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "Name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      alert("Email is Invalid.");
+      return;
+    }
+
+    if (!Name) {
+      alert("Field is Required.");
+      return;
+    }
+
+    if (!message) {
+      alert("Field is Required.");
+      return;
+    }
+
+    if (email || Name || message) {
+      setSuccessMessage("Form Submitted Successfully!");
+      return;
+    }
+
+    setName("");
+    setMessage("");
+    setEmail("");
+  };
+}
+
+export default Form;
