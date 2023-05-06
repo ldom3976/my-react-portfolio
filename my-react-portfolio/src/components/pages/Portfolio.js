47cce2216b1d341647c85de7495e45ba26a66eaa
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 14:12:59 -0400

    added bones for portfolio page

--- my-react-portfolio/src/components/pages/Portfolio.js
+++ my-react-portfolio/src/components/pages/Portfolio.js
@@ -3,10 +3,48 @@ import React from 'react';
  export default function Portfolio() {
    return (
      <div>
-       <h1>Portfolio</h1>
-       <p>
-         My Portfolio.
-       </p>
+       <h2>Portfolio</h2>
+        <div>
+
+        </div>
+
+    <h3>Social Network API</h3>
+    <p>social network api description</p>
+
+    <div>
+
+    </div>
+
+    <h3>Note Taker</h3>
+    <p>note taker description</p>
+
+    <div>
+
+    </div>
+
+    <h3>Team Generator</h3>
+    <p>team generator description</p>
+
+    <div>
+
+    </div>
+
+    <h3>Work Day Scheduler</h3>
+    <p>work day scheduler description</p>
+
+    <div>
+
+    </div>
+
+    <h3>Weather Dashboard</h3>
+    <p>weather dashboard description</p>
+
+    <div>
+
+    </div>
+
+    <h3>Maps R Us</h3>
+    <p>mapsrus description</p>
      </div>
    );
  }
