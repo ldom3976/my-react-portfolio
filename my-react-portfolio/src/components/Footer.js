951897481093113c3915652301ad3e2589f09500
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 13:53:10 -0400

    added footer

---
+++ my-react-portfolio/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ function Footer() {
+   return (
+     <ul className="nav">
+       <li className="nav-item">
+       <a href="https://github.com/ldom3976">
+        GitHub
+       </a>
+       </li>
+       </ul>
+       );
+    }
+
+    export default Footer;
