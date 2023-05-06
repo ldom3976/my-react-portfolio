21d6eef9fda201cdfd253c267d4c0ce1d66f3d95
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 13:40:45 -0400

    added resume page

--- my-react-portfolio/src/components/PortfolioContainer.js
+++ my-react-portfolio/src/components/PortfolioContainer.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 import Footer from './Footer';
 import '../style/style.css';
 
