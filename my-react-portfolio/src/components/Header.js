19cde8d4db21108230bc753480cb2f6a13ceb6e7
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 13:38:31 -0400

    added code for header

--- my-react-portfolio/src/components/Header.js
+++ my-react-portfolio/src/components/Header.js
@@ -1,1 +1,54 @@
 import React from 'react';
+
+//will probably need to go back through and change as I style
+
+function Header({ currentPage, handlePageChange }) {
+    return (
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+
+              //checking to see if current page is the About page
+
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#resume"
+                  onClick={() => handlePageChange('Resume')}
+
+                  className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                  >
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            );
+          }
+         
+          export default Header;
