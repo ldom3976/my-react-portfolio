5a1f81cdaaceeb1a93e4af0ff1054b4c86180751
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-06 13:26:09 -0400

    added picture and info on about page

--- my-react-portfolio/src/components/pages/About.js
+++ my-react-portfolio/src/components/pages/About.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ElishaBallard from "../../img/elishaballard.jpg";
 
  export default function About() {
    return (
      <div>
-       <h1>About Me</h1>
+       <h2>About Me</h2>
+       <div>
+        <img src={ElishaBallard} alt="Elisha Ballard" />
+       </div>
        <p>
-         About me.
+         I am a UNC Chapel Hill Bootcamp student who's currently learning full-stack web development. Cuurently 
+         I work as a contracted corporate and leisure reservation specialist. I am an ambitious and self-motivated
+         individual who is able to adapt quickly and thrive in fast-paced, deadline driven environments. I work closely with clients
+         to provide personalized experiences, and am able to work efficiently individually and as part of a team.
+        As a coding bootcamp student, I am constantly learning and improving my skills and collaborating with my peers.
        </p>
      </div>
    );
