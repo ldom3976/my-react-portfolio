42d51069b5464e43d0544a146f178a6d11fbe51c
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-07 12:37:28 -0400

    updated resume page

--- my-react-portfolio/src/components/pages/Resume.js
+++ my-react-portfolio/src/components/pages/Resume.js
@@ -1,12 +1,52 @@
 import React from 'react';
 
+const styles = {
+  h2: {
+    textAlign: "center",
+  },
+  link: {
+    color: "#FF7875FF",
+  },
+  space: {
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
+  },
+  h3: {
+    paddingTop: "0.75rem",
+  },
+};
+
  export default function Resume() {
    return (
-     <div>
-       <h1>Resume</h1>
+    <div style={styles.space}>
+       <h2 style={styles.h2}>Resume</h2>
        <p>
          My Resume.
        </p>
+
+      <h3 style={styles.h3}>Technical Skills Learned through Coding Bootcamp</h3>
+      <p>
+        <h4>Front-End</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>JQuery</li>
+          <li>Bootstrap</li>
+          <li>React</li>
+        </ul>
+      </p>
+      <p>
+        <h4>Back-End</h4>
+        <ul>
+          <li>MySQL</li>
+          <li>MongoDB & Mongoose</li>
+          <li>Express</li>
+          <li>Node</li>
+          <li>APIs</li>
+        </ul>
+      </p>
+      
      </div>
    );
  }
