657ad2ce5b6ee5cef599b91be8bbb4d1cd20b20f
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-07 14:02:58 -0400

    added some styling to portfolio page, need to add cards

--- my-react-portfolio/src/components/pages/Portfolio.js
+++ my-react-portfolio/src/components/pages/Portfolio.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from "react";
+import SocialNetwork from "../../img/socialnetapi.png";
+import NoteTaker from "../../img/notetakerscreen.png";
+import TeamGenerator from "../../img/teamscreen.png";
+import WorkSched from "../../img/wrkdsc.jpeg";
+import WeatherDash from "../../img/weather-dashboard.png";
+import MapsRUs from "../../img/mapsrusscreen.png";
+
+const styles = {
+  h2: {
+    textAlign: "center",
+  },
+  space: {
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
+  },
+  header: {
+    textAlign: "center",
+    paddingBottom: "0.15rem",
+  },
+  repo: {
+    textAlign: "center",
+    color: "#FF7875FF",
+    paddingBottom: "0.15rem",
+  },
+};
 
  export default function Portfolio() {
    return (
-     <div>
-       <h2>Portfolio</h2>
-        <div>
 
-        </div>
+    <div>
+      <h2 style={styles.h2}>Portfolio</h2>
+      
+        
 
     <h3>Social Network API</h3>
     <p>social network api description</p>
