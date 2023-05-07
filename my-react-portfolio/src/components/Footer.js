c5339ece9025569c97cab1f4d2602126d00a0543
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-05-07 13:37:14 -0400

    added icons and styling for footer

--- my-react-portfolio/src/components/Footer.js
+++ my-react-portfolio/src/components/Footer.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import Github from "../img/github.png";
+import StackOverflow from "../img/stackoverflow.png";
 
 const styles = {
   icons: {
     borderRadius: '25%',
     filter: 'opacity(100%)',
     margin: '5px',
+    width: '5%'
   },
 };
 
  function Footer() {
    return (
-     <ul className="nav">
-       <li className="nav-item">
+    <footer className="text-center">
+      <div className="container p-4">
+        <section className="mb-4">
+
        <a href="https://github.com/ldom3976" target="blank">
-        GitHub
-       </a>
-       </li>
-       </ul>
+       <img style={styles.icons} src={Github} alt="github icon" />
+        </a>
+
+        <a href="https://stackoverflow.com/users/21844526/" target="blank">
+       <img style={styles.icons} src={StackOverflow} alt="stack overflow icon" />
+        </a>
+
+          </section>
+          </div>
+          </footer>
        );
     };
 
